b03ea2138d10882961776799d6f447aca4da7e98
Gustavo Oliveira <gufonseca.oliveira@gmail.com>
2025-06-28 13:52:24 -0300

    implement form submission and data storage functionality

--- index.js
+++ index.js
@@ -3,9 +3,51 @@ const mongoose =  require("mongoose");
 const port = 8080;
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+mongoose.connect("mongodb://localhost/projectDG", {
+    userNewUrlParser: true, 
+    useUnifiedTopology: true,
 });
+let db = mongoose.connection;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: True }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.set({
+        "Allow-access-Allow-Origin": "*",
+    });
+    return res.redirect("index.html");
+});
+
+app.post("/formFillIp", (req, res) => {
+    const name = req.body.name;
+    const reason = req.body.reason;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const city = req.body.city;
+    const state = req.body.state;
+    const addressLine = req.body.addressLine;
+
+    const data = {
+        name: name,
+        reason: reason,
+        email: email,
+        phone: phone,
+        city: city,
+        state: state,
+        addressLine: addressLine,
+    };
+
+    db.collection("users").insertOne(data, (err, collection) => {
+        if (err) {
+            throw err;
+        }
+        console.log("Data inserted successfully.");
+    });
+
+    return res.redirect("formSubmitted.html");
+})
 
 app.listen(port, () => {
     console.log(`The application started successfully on port ${port}`);
